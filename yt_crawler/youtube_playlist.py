0259b40173d2247f27ea3c0f2560a13b61773206
luc_pimentel <lucas_cpimentel@proton.me>
2025-08-20 18:19:26 -0300

    feat: add get_playlist_details method to extract YouTube playlist metadata

--- yt_crawler/youtube_playlist.py
+++ yt_crawler/youtube_playlist.py
@@ -36,3 +36,31 @@ class PlaylistMixin:
         }
         
         return playlist_videos
+
+
+    def get_playlist_details(self, playlist_id: str) -> dict[str, Any]:
+        """
+        Get playlist details from YouTube playlist ID
+        
+        Args:
+            playlist_id (str): YouTube playlist ID
+            
+        Returns:
+            dict: Playlist details with first 2 keys wrapped in 'playlist_details' key
+        """
+        # Get the webpage content
+        url = f"https://www.youtube.com/playlist?list={playlist_id}"
+        scripts = extract_youtube_page_scripts(url, headers=HEADERS)
+        
+        playlist_data_dict = grab_dict_by_key(scripts, 'pageHeaderViewModel').get('pageHeaderViewModel')
+        
+        # Keep only the title and metadata keys from the playlist data
+        if playlist_data_dict:
+            keys_to_keep = ['title', 'metadata']
+            filtered_data = {key: playlist_data_dict[key] for key in keys_to_keep if key in playlist_data_dict}
+        else:
+            filtered_data = {}
+        
+        # Wrap in playlist_details dictionary
+        return {'playlist_details': filtered_data}
+
